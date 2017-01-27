b8776aa67fc7b8933110cd178dbdda1fb0673711
Jimmy Kuruvilla <jimmyjk@gmail.com>
2017-01-26 21:33:57 -0600

    Finish up menu style

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
-gulp.task('serve', function() {
-  browserSync({
+gulp.task('scss', function() {
+  return gulp.src('app/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/styles'))
+    .pipe(reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
     server: {
       baseDir: 'app'
-    }
-  });
+    },
+  })
+})
 
-  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+gulp.task('watch', ['browserSync', 'scss'], function() {
+  gulp.watch('app/styles/**/*.scss', ['scss']);
+  gulp.watch('app/html/**/*.html', reload);
+  gulp.watch('app/js/**/*.js', reload);
 });
