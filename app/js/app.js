ab73eae7fad4ba0f0714839edc46401b32a81f3a
Jimmy Kuruvilla <jimmyjk@gmail.com>
2017-05-25 22:33:59 +0100

    Maxwell engine repair

--- app/js/app.js
+++ app/js/app.js
@@ -1,89 +1,46 @@
 'use strict';
 
-console.warn('hia');
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  );
+};
 
 function init() {
-  var modal = $('#modal-content');
-//load templates programatically
-//add  click listeners. 
-  setTimeout(function() {
-    modal.load('../html/welcomeModal.html')
-    modal.on('click', 'li#about', function(e) {
-      console.log(e)
-      modal.load('../html/about.html')
+  var content = $('#content');
+  var menuList = ['home', 'about', 'directions', 'contact', 'resources', 'forsale']
+  var state = 'home';
+  var hash = window.location.hash;
+  if (hash) {
+    var passedState = hash.slice(1);
+    if (_.contains(menuList, passedState)) {
+      state = passedState;
+    }
+  }
 
-    });
+  content.addClass('visible');
+  content.load('../html/tag.html');
+  setTimeout(function () {
+    content.removeClass('visible');
+    setTimeout(function () {
+      content.load('../html/' + state + '.html');
+      content.addClass('visible');
+    }, 1000);
+  }, 2000)
 
-  }, 1000);
-
-
+  setMenuListeners(menuList, content);
 }
 
 init();
 
-
-function createMenu(){
-var menuItems = ['About', 'News', 'Screenshots'];
-// var menu = _.template("<li id='<%print()%>'> </li>");
-var menu = '';
-$.each(menuItems, function(index, item){
-  menu += '<li id="">' + item '</li>'
-});
-  <li id="about">
-                        About
-                    </li>
-                    <li>
-                        News
-                    </li>
-                    <li>
-                        Screenshots
-                    </li>
-                    <li>
-                        Live CD/USB CD/USB Key vendors Related Live CD
-                    </li>
-                    <li>
-                        Live Docs
-                    </li>
-                    <li>
-                        Server Edition
-                    </li>
-                    <li>
-                        Downloads
-                    </li>
-                    <li>
-                        DRBL-winroll
-                    </li>
-                    <li>
-                        Testimonials
-                    </li>
-                    <li>
-                        Lecture Materials
-                    </li>
-                    <li>
-                        Related Articles
-                    </li>
-                    <li>
-                        Partners
-                    </li>
-                    <li>
-                        FAQ/Q&A
-                    </li>
-                    <li>
-                        Forum
-                    </li>
-                    <li>
-                        Mailing Lists
-                    </li>
-                    <li>
-                        Developers
-                    </li>
-                    <li>
-                        Contributors
-                    </li>
-                    <li>
-                        Related Links
-                    </li>
-                    <li>
-                        Local Communities
-                    </li>
+function setMenuListeners(menuList, rootElem) {
+  $.each(menuList, function (i, selector) {
+    $('#' + selector).on('click', function () {
+      rootElem.load('../html/' + selector + '.html');
+      window.location.hash = selector;
+    });
+  })
 }
